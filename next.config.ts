9d5e9610fba463f378bc94f83ffdf99c433d2e1c
Fatih <fatihexi41@gmail.com>
2026-03-31 19:55:44 +0300

    Haberpik yayına girsin artik inat etme

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // KANKA: TypeScript hataları olsa bile Vercel'in siteyi kurmasına izin verir.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Eslint hatalarını da görmezden gel kanka, kafamız rahat olsun.
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
